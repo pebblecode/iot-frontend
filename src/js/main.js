4f569197c4866dd0a42e1be7d71f2af88bec567c
itomblack <itomblack@Ians-MacBook-Pro.local>
2016-04-15 17:29:35 +0100

    app washing machine to page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
 (function () { 'use strict';
 
-  console.log('test');
+  var s = Snap("#svg-1");
+
+  var floor = s.rect(0, 310, 400, 100)
+  .attr({
+    fill: "#ccf3ff"
+  });
+
+  Snap.load("../img/washing-machine.svg", function (f) {
+    // f.selectAll("svg");
+    var g = f.select("#washer");
+    s.append(g);
+    g.drag();
+  });
 
 
 
